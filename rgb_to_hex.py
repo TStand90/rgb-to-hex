bfb3c0bc68e10a59aefa3327e40be3e308666106
Tyler Standridge <tyler@tylers-macbook.attlocal.net>
2015-03-17 22:54:40 -0700

    Added range check to prevent numbers outside RGB range

--- rgb_to_hex.py
+++ rgb_to_hex.py
@@ -17,9 +17,30 @@ def main():
         rgb_green = int(green)
         rgb_blue = int(blue)
 
-        hex_string = "#%02x%02x%02x".upper() % (rgb_red, rgb_green, rgb_blue)
+        range_errors = []
 
-        print(hex_string)
+        if not number_in_range(rgb_red):
+            range_errors.append(red)
+        if not number_in_range(rgb_green):
+            range_errors.append(green)
+        if not number_in_range(rgb_blue):
+            range_errors.append(blue)
+
+        if range_errors:
+            errors = ', '.join(set(range_errors))
+            print("Error: %s not in range 0-255" % errors)
+        else:
+            hex_string = '#%02x%02x%02x'.upper() % (rgb_red,
+                                                    rgb_green,
+                                                    rgb_blue)
+            print(hex_string)
+
+
+def number_in_range(number):
+    if number in range(256):
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
